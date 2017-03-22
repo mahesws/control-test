9cee6c69c39866699ab37dae095e5a033999c5fb
Jarret Lavallee <jarret.lavallee@gmail.com>
2017-03-22 10:09:51 -0700

    Remove eyaml and deep_merge
    Due to performance issues with the initial startup

--- site/profile/manifests/puppetmaster.pp
+++ site/profile/manifests/puppetmaster.pp
@@ -2,16 +2,16 @@ class profile::puppetmaster (
   $hiera_yaml = "${::settings::confdir}/hiera.yaml"
 ){
   # Default Packages
-  package { 'hiera-eyaml':
-    ensure   => present,
-    provider => puppet_gem,
-    notify   => Service['pe-puppetserver'],
-  }
-  package { 'deep_merge':
-    ensure   => present,
-    provider => puppetserver_gem,
-    notify   => Service['pe-puppetserver'],
-  }
+  #package { 'hiera-eyaml':
+    #ensure   => present,
+    #provider => puppet_gem,
+    #notify   => Service['pe-puppetserver'],
+    #}
+    #package { 'deep_merge':
+      #ensure   => present,
+      #provider => puppetserver_gem,
+      #notify   => Service['pe-puppetserver'],
+      #}
   # Metric Collection
   include pe_metric_curl_cron_jobs
   # Hiera Data
@@ -22,9 +22,9 @@ class profile::puppetmaster (
       'osfamily/%{::osfamily}',
       'common',
     ],
-    eyaml      => true,
+    #eyaml      => true,
     hiera_yaml => $hiera_yaml,
-    merge_behavior => 'deeper',
+    #merge_behavior => 'deeper',
     datadir    => '/etc/puppetlabs/code/environments/%{environment}/hieradata',
     owner      => 'pe-puppet',
     group      => 'pe-puppet',
